dbe40c992db71d2b8b46278ae1975d729e248a89
Jackie Janssen <jackiejanssen@icloud.com>
2019-04-11 16:43:59 +0200

    Convert to MVC

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jackie Janssen on 11/04/19.
+//  Copyright © 2019 Jackie Janssen. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
